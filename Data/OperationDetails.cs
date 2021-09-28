63374930ade6d5d896afce1309164fbb57e21316
Jhorunn <crperezz92@gmail.com>
2021-09-28 14:24:29 -0500

    First Commit

---
+++ Data/OperationDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsCallBack.Data
+{
+	public class OperationDetails
+	{
+		public List<int> NumberList { get; set; }
+		public string sNumberList { get; set; }
+	}
+}
---
+++ Utils/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsCallBack.Utils
+{
+	public static class ExtensionMethods
+	{
+		public static string ExtendToString(this List<int> list)
+		{
+			string result = "";
+			foreach (int number in list)
+			{
+				result = $"{result} {number}";
+			}
+			return result;
+		}
+	}
+}
